c65448966c19d725d9c40136d908be2fcab0d8d5
Chintamani Sahoo <116138185+rajxd0@users.noreply.github.com>
2023-01-05 10:29:49 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,6 +2,7 @@ from PIL import Image
 from logging import getLogger, FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info, warning as log_warning
 from socket import setdefaulttimeout
 from urllib.request import urlretrieve
+from telegram.ext import Updater as tgUpdater
 from faulthandler import enable as faulthandler_enable
 from qbittorrentapi import Client as qbClient
 from aria2p import API as ariaAPI, Client as ariaClient
@@ -917,7 +918,6 @@ else:
             del qb_opt[k]
     qb_client.app_set_preferences(qb_opt)
 
-tgDefaults = Defaults(parse_mode='HTML', disable_web_page_preview=True, allow_sending_without_reply=True, run_async=True)
 updater = tgUpdater(token=BOT_TOKEN, defaults=tgDefaults, request_kwargs={'read_timeout': 20, 'connect_timeout': 15})
 bot = updater.bot
 dispatcher = updater.dispatcher
